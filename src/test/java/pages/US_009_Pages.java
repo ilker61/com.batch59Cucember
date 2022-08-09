20e1661942999e4b0c2d62574f01f0fca856375f
ilker61 <jakyuer@gmail.com>
2022-08-09 13:09:39 +0200

    09.08.22

---
+++ src/test/java/pages/US_009_Pages.java
@@ -0,0 +1,18 @@
+package pages;
+
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
+public class US_009_Pages {
+
+    public US_009_Pages(){
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+
+
+
+
+
+
+}
